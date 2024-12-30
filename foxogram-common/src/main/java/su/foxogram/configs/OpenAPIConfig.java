92234f0546c28b54e1ed5dcff2dab8a4cd7638c7
daniel <danil.ch9@proton.me>
2024-12-30 17:20:15 +0300

    Add openapi servers definition

--- foxogram-common/src/main/java/su/foxogram/configs/OpenAPIConfig.java
+++ foxogram-common/src/main/java/su/foxogram/configs/OpenAPIConfig.java
@@ -5,8 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -19,7 +23,12 @@ public class OpenAPIConfig {
 	@Bean
 	public OpenAPI openAPI() {
 		Info info = new Info().title("Foxogram").version(apiConfig.getVersion());
+
+		List<Server> servers = Arrays.asList(new Server().url("https://api.dev.foxogram.su").description("Development"),
+				new Server().url("https://api.foxogram.su").description("Production"));
+
 		SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
+
 		Components components = new Components()
 				.addSecuritySchemes("Authorization", new SecurityScheme()
 						.type(SecurityScheme.Type.HTTP)
@@ -28,6 +37,7 @@ public class OpenAPIConfig {
 
 		return new OpenAPI()
 				.info(info)
+				.servers(servers)
 				.addSecurityItem(securityRequirement)
 				.components(components);
 	}
