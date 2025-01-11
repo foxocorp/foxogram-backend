05a83e7abf697c852c02d3f63637fa27ae209dd7
daniel <danil.ch9@proton.me>
2025-01-11 22:38:01 +0300

    Change camel case to snake case

--- foxogram-api/src/main/java/su/foxogram/configs/WebConfig.java
+++ foxogram-api/src/main/java/su/foxogram/configs/WebConfig.java
@@ -3,7 +3,6 @@ package su.foxogram.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import su.foxogram.interceptors.AuthenticationInterceptor;
@@ -12,7 +11,6 @@ import su.foxogram.interceptors.MemberInterceptor;
 import su.foxogram.interceptors.RateLimitInterceptor;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	private final AuthenticationInterceptor authenticationInterceptor;
 
