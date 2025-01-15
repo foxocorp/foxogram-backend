a3cdc9787a5c42d20775509524cbde7d7559ab0b
nelifs <nelifs@icloud.com>
2025-01-16 00:33:47 +0300

    Fix members fetch

--- foxogram-common/src/main/java/su/foxogram/models/Channel.java
+++ foxogram-common/src/main/java/su/foxogram/models/Channel.java
@@ -34,7 +34,7 @@ public class Channel {
 	@JoinColumn(name = "user_id", nullable = false)
 	public User owner;
 
-	@OneToMany(mappedBy = "channel", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "channel", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
 	private List<Member> members;
 
 	@OneToMany(mappedBy = "channel", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
