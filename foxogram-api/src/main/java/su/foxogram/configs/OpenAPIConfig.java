105b4fd40da7ff26cb6e6a7aa512ff162a7ae8a1
daniel <danil.ch9@proton.me>
2025-01-16 14:20:32 +0300

    Add external docs to openapi

--- foxogram-api/src/main/java/su/foxogram/configs/OpenAPIConfig.java
+++ foxogram-api/src/main/java/su/foxogram/configs/OpenAPIConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.core.jackson.ModelResolver;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -27,11 +28,20 @@ public class OpenAPIConfig {
 
 	@Bean
 	public OpenAPI openAPI() {
-		Info info = new Info().title("Foxogram").version(apiConfig.getVersion());
+		Info info = new Info()
+				.title("Foxogram")
+				.version(apiConfig.getVersion());
 
-		List<Server> servers = Arrays.asList(new Server().url("https://api.dev.foxogram.su").description("Development"),
-				new Server().url("https://api.foxogram.su").description("Production"));
+		ExternalDocumentation externalDocs = new ExternalDocumentation()
+				.description("External documentation")
+				.url("https://github.com/foxocorp/foxogram-docs");
 
+		List<Server> servers = Arrays.asList(
+				new Server().url("https://api.dev.foxogram.su").description("Development"),
+				new Server().url("https://api.foxogram.su").description("Production")
+		);
+
+		// Enable bearer authorization
 		SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
 
 		Components components = new Components()
@@ -40,6 +50,7 @@ public class OpenAPIConfig {
 						.scheme("bearer")
 						.bearerFormat("JWT"));
 
+		// Disable constructor fields and set snake_case
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
@@ -50,6 +61,7 @@ public class OpenAPIConfig {
 
 		return new OpenAPI()
 				.info(info)
+				.externalDocs(externalDocs)
 				.servers(servers)
 				.addSecurityItem(securityRequirement)
 				.components(components);
