38eca1e511f9a28fcdd494aa4c98065469084387
krabiworld <krabiworld@proton.me>
2025-05-09 17:37:59 +0300

    Remove unused caching annotations

--- foxogram-common/src/main/java/su/foxogram/Main.java
+++ foxogram-common/src/main/java/su/foxogram/Main.java
@@ -3,9 +3,7 @@ package su.foxogram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Main {
