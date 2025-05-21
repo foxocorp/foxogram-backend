bf3b35055aebc6184d6a22f2807db66df10f1c89
nelifs <nelifs@icloud.com>
2025-05-21 13:44:37 +0300

    Add rabbitmq

---
+++ foxogram-api/src/main/java/su/foxogram/listener/RabbitListener.java
@@ -0,0 +1,34 @@
+package su.foxogram.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import su.foxogram.dto.gateway.StatusDTO;
+import su.foxogram.service.UserService;
+
+@Slf4j
+@Component
+public class RabbitListener {
+
+	private final ObjectMapper objectMapper;
+
+	private final UserService userService;
+
+	@Autowired
+	public RabbitListener(ObjectMapper objectMapper, UserService userService) {
+		this.objectMapper = objectMapper;
+		this.userService = userService;
+	}
+
+	@org.springframework.amqp.rabbit.annotation.RabbitListener(queues = "${rabbit.queue}")
+	public void listen(String in) throws Exception {
+		StatusDTO dto = objectMapper.readValue(in, StatusDTO.class);
+
+		long userId = dto.getUserId();
+		int status = dto.getStatus();
+
+		log.info("Got status message for {} with status {}", userId, status);
+		userService.setStatus(userId, status);
+	}
+}
