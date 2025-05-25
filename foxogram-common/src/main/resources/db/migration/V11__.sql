ed3dfbaa081d51dedd7965c4cb05b2b5ed1f8570
krabiworld <krabiworld@proton.me>
2025-05-25 17:19:54 +0300

    Fix v11 migration

--- foxogram-common/src/main/resources/db/migration/V11__.sql
+++ foxogram-common/src/main/resources/db/migration/V11__.sql
@@ -1,1 +1,2 @@
+DROP INDEX idx_user_contact;
 CREATE UNIQUE INDEX idx_user_contact ON user_contacts (user_id, contact_id);
--- foxogram-common/src/main/resources/db/migration/V9__.sql
+++ foxogram-common/src/main/resources/db/migration/V9__.sql
@@ -11,4 +11,3 @@ CREATE TABLE user_contacts
 );
 
 CREATE INDEX idx_user_contact ON user_contacts (user_id, contact_id);
-
