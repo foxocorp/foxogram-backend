676b1b2c4394b7e10e0fa09eed1d31a421a7fb12
nelifs <nelifs@icloud.com>
2025-05-26 17:32:16 +0300

    Refactor a project to monolith

---
+++ src/main/java/su/foxogram/handler/HelloHandler.java
@@ -0,0 +1,54 @@
+package su.foxogram.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import su.foxogram.constant.GatewayConstant;
+import su.foxogram.constant.UserConstant;
+import su.foxogram.dto.gateway.EventDTO;
+import su.foxogram.dto.gateway.response.HelloDTO;
+import su.foxogram.handler.structure.BaseHandler;
+import su.foxogram.model.Session;
+import su.foxogram.service.AuthenticationService;
+import su.foxogram.service.UserService;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+public class HelloHandler implements BaseHandler {
+
+	private final AuthenticationService authenticationService;
+
+	private final ObjectMapper objectMapper;
+
+	private final UserService userService;
+
+	public HelloHandler(AuthenticationService authenticationService, ObjectMapper objectMapper, UserService userService) {
+		this.authenticationService = authenticationService;
+		this.objectMapper = objectMapper;
+		this.userService = userService;
+	}
+
+	@Override
+	public int getOpcode() {
+		return GatewayConstant.Opcode.IDENTIFY.ordinal();
+	}
+
+	@Override
+	public void handle(WebSocketSession session, ConcurrentHashMap<String, Session> sessions, EventDTO payload) throws Exception {
+		String accessToken = ((Map<String, String>) payload.getD()).get("token");
+
+		long userId = authenticationService.getUser(accessToken, false).getId();
+		Session userSession = sessions.get(session.getId());
+		userSession.setUserId(userId);
+		userSession.setLastPingTimestamp(System.currentTimeMillis());
+
+		session.sendMessage(new TextMessage(objectMapper.writeValueAsString(new HelloDTO())));
+		userService.setStatus(userId, UserConstant.Status.ONLINE.getStatus());
+		log.info("Authenticated session ({}) with user id {}", session.getId(), userId);
+	}
+}
---
+++ src/main/java/su/foxogram/service/WebSocketService.java
@@ -0,0 +1,61 @@
+package su.foxogram.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import su.foxogram.dto.gateway.EventDTO;
+import su.foxogram.handler.structure.EventHandler;
+import su.foxogram.model.Session;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Service
+public class WebSocketService {
+
+	private final EventHandler webSocketHandler;
+
+	private final ObjectMapper objectMapper;
+
+	public WebSocketService(EventHandler webSocketHandler, ObjectMapper objectMapper) {
+		this.webSocketHandler = webSocketHandler;
+		this.objectMapper = objectMapper;
+	}
+
+	public void sendMessageToAll(int opcode, Map<String, Object> data, String type) throws Exception {
+		ConcurrentHashMap<String, Session> sessions = webSocketHandler.getSessions();
+		for (Session session : sessions.values()) {
+			WebSocketSession wsSession = session.getWebSocketSession();
+
+			int seqNumber = session.getSequence();
+			session.increaseSequence();
+
+			if (!wsSession.isOpen()) return;
+
+			wsSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(new EventDTO(opcode, data, seqNumber, type))));
+			log.debug("Sent message to all sessions (opcode: {}, type: {})", opcode, type);
+		}
+	}
+
+	public void sendMessageToSessions(List<Long> userIds, int opcode, Object data, String type) throws Exception {
+		ConcurrentHashMap<String, Session> sessions = webSocketHandler.getSessions();
+		for (Session session : sessions.values()) {
+			if (session != null) {
+				if (!userIds.contains(session.getUserId())) return;
+
+				int seqNumber = session.getSequence();
+				session.increaseSequence();
+				WebSocketSession wsSession = session.getWebSocketSession();
+
+				if (!wsSession.isOpen()) return;
+
+				wsSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(new EventDTO(opcode, data, seqNumber, type))));
+				log.debug("Sent message to userIds ({}) with (opcode: {}, type: {})", userIds, opcode, type);
+			}
+		}
+	}
+}
