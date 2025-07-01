d11ccdb0734a5d4baa7400c9d66e89c60b28d8a2
nelifs <nelifs@icloud.com>
2025-07-01 17:47:26 +0300

    Add ability to delete channel icon

--- src/main/java/app/foxochat/constant/ValidationConstant.java
+++ src/main/java/app/foxochat/constant/ValidationConstant.java
@@ -54,6 +54,8 @@ public class ValidationConstant {
 		public static final String USER_AVATAR_MUST_BE_POSITIVE = "User avatar id must be positive";
 
 		public static final String USER_BANNER_MUST_BE_POSITIVE = "User banner id must be positive";
+
+		public static final String CHANNEL_ICON_MUST_BE_POSITIVE = "Channel icon id must be positive";
 	}
 
 	public static class Regex {
