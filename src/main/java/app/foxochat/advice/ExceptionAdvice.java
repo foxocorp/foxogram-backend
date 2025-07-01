d81eb515d4abe127c3da04b7a4f690f23e2206b7
nelifs <nelifs@icloud.com>
2025-07-01 17:06:49 +0300

    Add spring boot exceptions messages

--- src/main/java/app/foxochat/advice/ExceptionAdvice.java
+++ src/main/java/app/foxochat/advice/ExceptionAdvice.java
@@ -11,11 +11,14 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -23,6 +26,12 @@ import java.util.stream.Collectors;
 public class ExceptionAdvice {
 
 	private final APIConfig apiConfig;
+
+	private final Set<Class<?>> SPRING_BOOT_EXCEPTIONS = Set.of(
+			HttpMessageNotReadableException.class,
+			HttpMediaTypeNotSupportedException.class,
+			ServletRequestBindingException.class
+	);
 
 	public ExceptionAdvice(APIConfig apiConfig) {
 		this.apiConfig = apiConfig;
@@ -36,11 +45,6 @@ public class ExceptionAdvice {
 	@ExceptionHandler(BaseException.class)
 	public ResponseEntity<ExceptionDTO> handleBaseException(BaseException exception) {
 		return buildErrorResponse(exception.getErrorCode(), exception.getMessage(), exception.getStatus());
-	}
-
-	@ExceptionHandler(HttpMessageNotReadableException.class)
-	public ResponseEntity<ExceptionDTO> handleHttpMessageNotReadable() {
-		return buildErrorResponse(ExceptionConstant.API.EMPTY_BODY.getValue(), ExceptionConstant.Messages.REQUEST_BODY_EMPTY.getValue(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -71,7 +75,8 @@ public class ExceptionAdvice {
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ExceptionDTO> handleException(Exception exception) {
 		String message = exception.getMessage();
-		if (!apiConfig.isDevelopment()) message = ExceptionConstant.Messages.INTERNAL_ERROR.getValue();
+		if (!apiConfig.isDevelopment() || SPRING_BOOT_EXCEPTIONS.stream().noneMatch(e -> e.isAssignableFrom(exception.getClass())))
+			message = ExceptionConstant.Messages.INTERNAL_ERROR.getValue();
 
 		log.error(ExceptionConstant.Messages.SERVER_EXCEPTION_STACKTRACE.getValue(), exception);
 		return buildErrorResponse(ExceptionConstant.Unknown.ERROR.getValue(), message, HttpStatus.INTERNAL_SERVER_ERROR);
