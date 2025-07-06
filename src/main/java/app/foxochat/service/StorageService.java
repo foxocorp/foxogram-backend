047ae12ffba74e2ac82169af23e0e52a186ff327
nelifs <nelifs@icloud.com>
2025-07-07 00:02:36 +0300

    Fix avatar/attachment upload

--- src/main/java/app/foxochat/service/StorageService.java
+++ src/main/java/app/foxochat/service/StorageService.java
@@ -1,8 +1,9 @@
 package app.foxochat.service;
 
 import app.foxochat.dto.internal.MediaPresignedURLDTO;
+import app.foxochat.exception.media.UploadFailedException;
 
 public interface StorageService {
 
-    MediaPresignedURLDTO getPresignedUrl(String bucketName);
+    MediaPresignedURLDTO getPresignedUrl(String bucketName) throws UploadFailedException;
 }
